d4fd04f92b4b7a90ee373c503c2310ac60ab7c70
scud <alban.escudier@hotmail.fr>
2023-01-03 17:08:25 +0100

    mise a jour du formulaire d'inscription avec la db

--- inscription.php
+++ inscription.php
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <!-- les liens -->
-        <?php include ("_head/link.php"); ?>
-        <title>Inscription</title>
-    </head>
-    <body>
-        
-        <!-- connexion bade de donnees -->
-        <?php
-        require_once ("include.php");
+ <!-- connexion bade de donnees -->
+<?php
+        require_once('include.php');
         //$db = new PDO('mysql:host=localhost;dbname=bddcryptosvrai', 'root', '');
-        ?>
 
-        
-        <!-- L'en-tête -->
-        <?php include("_header/header.php"); ?>
-        <?php
+
         if(!empty($_POST)){
             extract($_POST);
 
@@ -24,9 +11,9 @@
 
             
             if(isset($_POST['inscription'])){
+                $pseudo = trim($pseudo);
                 $mail = trim($mail);
                 $confmail = trim($confmail);
-                $pseudo = trim($pseudo);
                 $password = trim($password);
                 $confpassword = trim($confpassword);
                 $datenaissance = trim($datenaissance);
@@ -36,14 +23,23 @@
                 $pays = trim($pays);
                 
                 if(empty($pseudo)){
-                    $valid= false;
-                    $err_pseudo = "Ce champ ne peut pas être vide";
+                    $valid = false;
+                    $err_pseudo = "Ce champ ne peut pas être vide"; 
+                
+                }elseif(grapheme_strlen($pseudo) < 4){
+                    $valid = false;
+                    $err_pseudo = "Ce pseudo doit faire plus de 4 caractères"; 
+                }elseif(grapheme_strlen($pseudo) > 25){
+                    $valid = false;
+                    $err_pseudo = "Ce pseudo doit faire moins de 26 caractères(" . grapheme_strlen($pseudo) . "/25";
                 }else{
-                    $req = $DB->prepare("SELECT id FROM utilisateur WHERE pseudo = ?");
+                    $req = $DB->prepare("SELECT id 
+                    FROM utilisateur 
+                    WHERE pseudo = ?");
                 
                     $req->execute(array($pseudo));
 
-                    $req = $req->feetch();
+                    $req = $req->fetch();
 
                     if(isset($req['id'])){
                         $valid = false;
@@ -91,77 +87,97 @@
                     $req = $DB->prepare("INSERT INTO utilisateur(pseudo, mail, mdp, date_creation, date_connexion) VALUES (?, ?, ?, ?, ?)");
                     $req->execute(array($pseudo, $mail, $crypt_password, $date_creation, $date_creation));
 
-                    header('Location: connexion.php');
+                    header('Location: /connexion.php');
                     exit;
+
                 }else{
-                    echo 'nok';
+                    echo "regarde ce qui va pas il y a une faute";
                     //pas d'insertion (affichage message d'erreur)
                 }
             
             }
         }
-       ?>
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- les liens -->
+        <?php include ("_head/link.php"); ?>
+        <title>Inscription</title>
+    </head>
+    <body>
+        
+       
+
+        
+        <!-- L'en-tête -->
+        <?php include("_header/header.php"); ?>
+        
         <p class="text-5xl text-center">Inscription</p>
         <!-- debut formulaire d'inscription -->
         
         <div class="container">
-        
-            <form method ="post">
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6">
+                    <form method ="post">
 
-                <div class="mb-3">
-                    <label class="form-label">Pseudo :</label>
-                    <input class="form-control" type="text" name="pseudo" value ="<?php if(isset($pseudo)){echo $pseudo;} ?>" placeholder="pseudo"/>
-                    <div class="text-red-500"> <?php if(isset($err_pseudo)){ echo $err_pseudo;}?> </div>
+                        <div class="mb-3">
+                            <label class="form-label">Pseudo :</label>
+                            <input class="form-control" type="text" name="pseudo" value ="<?php if(isset($pseudo)){ echo $pseudo; } ?>" placeholder="pseudo"/>
+                            <div class="text-red-500"> <?php if(isset($err_pseudo)){ echo $err_pseudo;}?> </div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Mail :</label>
+                            <input class="form-control" type="email" name="mail" value ="<?php if(isset($mail)){ echo $mail; } ?>" placeholder="mail"/>
+                            <div class="text-red-500"> <?php if(isset($err_mail)){ echo $err_mail;}?> </div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Confirmation du mail :</label>
+                            <input class="form-control" type="email" name="confmail" value ="<?php if(isset($confmail)){ echo $confmail; } ?>" placeholder="confirmation mail"/>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Mot de passe:</label>
+                            <input class="form-control" type="password" name="password" value ="<?php if(isset($password)){ echo $password; } ?>" placeholder="Mot de passe"/>
+                            <div class="text-red-500"> <?php if(isset($err_password)){ echo $err_password;}?> </div>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Confirmation du mot de passe:</label>
+                            <input class="form-control"  type="password" name="confpassword" value ="" placeholder="confirmation mot de passe"/>
+                        </div>
+                   <div class="mb-3">
+                            <label class="form-label">Date de naissance</label>
+                            <input class="form-control" type="date" name="datenaissance" value ="<?php if(isset($datenaissance)){echo $datenaissance;} ?>" placeholder="date de naissance"/>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Adresse</label>
+                            <input class="form-control" type="adress" name="adresse" value ="<?php if(isset($adresse)){echo $adresse;} ?>" placeholder="adresse"/>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Code postal</label>
+                            <input class="form-control" type="text" name="codepostal" value ="<?php if(isset($cp)){echo $cp;} ?>" placeholder="codepostal"/>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Ville</label>
+                            <input class="form-control" type="text" name="ville" value ="<?php if(isset($ville)){echo $ville;} ?>" placeholder="ville"/>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Pays</label>
+                            <input class="form-control" type="text" name="pays" value ="<?php if(isset($pays)){echo $pays;} ?>" placeholder="pays"/>
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" name="inscription" class="btn btn-outline-primary">S'inscrire</button>
+                        </div>
+                    </form>    
                 </div>
-                <div class="mb-3">
-                    <label class="form-label">Mail :</label>
-                    <input class="form-control" type="email" name="mail" value ="<?php if(isset($mail)){echo $mail;} ?>" placeholder="mail"/>
-                    <div class="text-red-500"> <?php if(isset($err_mail)){ echo $err_mail;}?> </div>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Confirmation du mail :</label>
-                    <input class="form-control" type="email" name="confmail" value ="<?php if(isset($confmail)){echo $confmail;} ?>" placeholder="confirmation mail"/>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Mot de passe:</label>
-                    <input class="form-control" type="password" name="password" value ="<?php if(isset($password)){echo $password;} ?>" placeholder="Mot de passe"/>
-                    <div class="text-red-500"> <?php if(isset($err_password)){ echo $err_password;}?> </div>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Confirmation du mot de passe:</label>
-                    <input class="form-control"  type="password" name="confpassword" value ="" placeholder="confirmation mot de passe"/>
-                </div>
-               <!--  <div class="mb-3">
-                    <label class="form-label">Date de naissance</label>
-                    <input class="form-control" type="date" name="datenaissance" value ="<?php if(isset($datenaissance)){echo $datenaissance;} ?>" placeholder="date de naissance"/>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Adresse</label>
-                    <input class="form-control" type="adress" name="adresse" value ="<?php if(isset($adresse)){echo $adresse;} ?>" placeholder="adresse"/>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Code postal</label>
-                    <input class="form-control" type="text" name="codepostal" value ="<?php if(isset($cp)){echo $cp;} ?>" placeholder="codepostal"/>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Ville</label>
-                    <input class="form-control" type="text" name="ville" value ="<?php if(isset($ville)){echo $ville;} ?>" placeholder="ville"/>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Pays</label>
-                    <input class="form-control" type="text" name="pays" value ="<?php if(isset($pays)){echo $pays;} ?>" placeholder="pays"/>
-                </div> -->
-                <div class="mb-3">
-                    <button type="submit" name="inscription" class="btn btn-outline-primary">S'inscrire</button>
-                </div>
-                
-            </form>
+            </div>
         </div>       
 
         
        
         <!-- Le pied de page -->
-        <?php include("_footer/footer.php"); ?>
+        <?php  include("_footer/footer.php"); ?>
         <!-- les scripts tailwinds et bootstrap -->
         <?php include("_head/script.php") ;?>
     </body>
